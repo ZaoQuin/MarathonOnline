3242a66c0904bc2800dbda7ffb2d4062627877d5
Nguyễn Hà Quỳnh Giao <21110171@student.hcmute.edu.vn>
2024-10-02 23:50:59 +0700

    Adjust flow

--- app/src/main/java/com/university/marathononline/login/LoginActivity.kt
+++ app/src/main/java/com/university/marathononline/login/LoginActivity.kt
@@ -16,6 +16,8 @@ import androidx.core.view.WindowInsetsCompat
 import com.university.marathononline.MainActivity
 import com.university.marathononline.R
 import com.university.marathononline.databinding.ActivityLoginBinding
+import com.university.marathononline.databinding.ActivityMainBinding
+import com.university.marathononline.record.RecordActivity
 
 class LoginActivity : AppCompatActivity() {
 
@@ -39,53 +41,58 @@ class LoginActivity : AppCompatActivity() {
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        passwordEditText = findViewById(R.id.passwordEditText)
-        usernameEditText = findViewById(R.id.usernameEditText)
-        val loginButton: Button = findViewById(R.id.loginButton)
-
-        passwordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.password_icon, 0)
-
-        passwordEditText.setOnTouchListener { _, event ->
-            if (event.action == MotionEvent.ACTION_UP) {
-                if (event.rawX >= (passwordEditText.right - passwordEditText.compoundDrawables[2].bounds.width())) {
-                    isPasswordVisible = !isPasswordVisible
-                    if (isPasswordVisible) {
-                        passwordEditText.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
-                        passwordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0,
-                            R.drawable.password_visible_off_icon, 0)
-                    } else {
-                        passwordEditText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
-                        passwordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0,
-                            R.drawable.password_icon, 0)
-                    }
-                    passwordEditText.setSelection(passwordEditText.text.length)
-                    return@setOnTouchListener true
-                }
-            }
-            false
+        binding.loginButton.setOnClickListener{
+            val intent = Intent(binding.root.context, MainActivity::class.java)
+            binding.root.context.startActivity(intent)
         }
+//
+//        passwordEditText = findViewById(R.id.passwordEditText)
+//        usernameEditText = findViewById(R.id.usernameEditText)
+//        val loginButton: Button = findViewById(R.id.loginButton)
+//
+//        passwordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.password_icon, 0)
+//
+//        passwordEditText.setOnTouchListener { _, event ->
+//            if (event.action == MotionEvent.ACTION_UP) {
+//                if (event.rawX >= (passwordEditText.right - passwordEditText.compoundDrawables[2].bounds.width())) {
+//                    isPasswordVisible = !isPasswordVisible
+//                    if (isPasswordVisible) {
+//                        passwordEditText.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+//                        passwordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0,
+//                            R.drawable.password_visible_off_icon, 0)
+//                    } else {
+//                        passwordEditText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+//                        passwordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0,
+//                            R.drawable.password_icon, 0)
+//                    }
+//                    passwordEditText.setSelection(passwordEditText.text.length)
+//                    return@setOnTouchListener true
+//                }
+//            }
+//            false
+//        }
 
-        loginButton.setOnClickListener {
-            val usernameInput = usernameEditText.text.toString()
-            val passwordInput = passwordEditText.text.toString()
+//        binding.loginButton.setOnClickListener {
+//            val usernameInput = usernameEditText.text.toString()
+//            val passwordInput = passwordEditText.text.toString()
+//
+//            if (usernameInput == user.username && passwordInput == user.password) {
+//                val intent = Intent(this, MainActivity::class.java)
+//                intent.putExtra("mockData", "This is some mock data")
+//                startActivity(intent)
+//                finish()
+//            } else {
+//                Toast.makeText(this, "Invalid credentials. Please try again.", Toast.LENGTH_SHORT).show()
+//            }
+//        }
 
-            if (usernameInput == user.username && passwordInput == user.password) {
-                val intent = Intent(this, MainActivity::class.java)
-                intent.putExtra("mockData", "This is some mock data")
-                startActivity(intent)
-                finish()
-            } else {
-                Toast.makeText(this, "Invalid credentials. Please try again.", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
 }
